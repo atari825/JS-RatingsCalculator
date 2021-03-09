b656d6830416717502f3ebade185317bdc1b7ed1
atari825 <ahtahreewoner@gmail.com>
2021-03-09 15:48:39 -0700

    Update ratings.js
    1

--- js/ratings.js
+++ js/ratings.js
@@ -1,7 +1,7 @@
 function collect_ratings() {
     let ratings = {counts:0, sum:0, average: 0 };
     let rating = 0;
-    let ratings = document.querySelectorAll(".rating");
+    document.querySelectorAll(".rating");
     const elements = [];
     elements.forEach (elememt => {
         element.replace("star", parseInt())
